0e1a8ba5c195f1f36da9871135f25b3a86cc2f8c
TrickyPi <33021497+TrickyPi@users.noreply.github.com>
2020-12-31 10:25:44 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello Webhook ! today
+          新年快乐！
         </p>
         <a
           className="App-link"
